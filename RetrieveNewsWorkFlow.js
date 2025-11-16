40c744e30e70a99a28f312dd43357594a5859bcf
rootVIII <colleyloyejames@gmail.com>
2025-11-16 01:25:40 -0500

    writes RITM to custom table

--- RetrieveNewsWorkFlow.js
+++ RetrieveNewsWorkFlow.js
@@ -1,5 +1,5 @@
-function main() {
-    var sections = {
+function getFormVars() {
+    return {
         World: current.variables.gnrfWorld.toString() === 'true',
         US: current.variables.gnrfUS.toString() === 'true',
         Business: current.variables.gnrfBusiness.toString() === 'true',
@@ -9,7 +9,40 @@ function main() {
         Science: current.variables.gnrfScience.toString() === 'true',
         Health: current.variables.gnrfHealth.toString() === 'true',
     };
+}
 
+function getNews(titles) {
+    var client = new sn_ws.RESTMessageV2('OkSurfNews', 'PostNewsSections');
+    var payload = JSON.stringify({
+        "sections": titles
+    });
+    client.setRequestBody(payload);
+    var resp = client.execute();
+    // load JSON into object if needed:
+    // var response = JSON.pasre(resp.getBody());
+    return resp;
+}
+
+function writeNewsTableEntry() {
+    var gr = new GlideRecord('u_google_news_requests');
+    gr.initialize();
+    gr.u_ritm = current.sys_id;
+    gr.insert();
+}
+
+function writeAttachment(response) {
+    var attachment = new Attachment();
+    attachment.write(
+        current.getTableName(),
+        current.sys_id,
+        current.number + '.json',
+        'text/plain',
+        response.getBody()
+    );
+}
+
+function main() {
+    var sections = getFormVars();
     var titles = [];
     for (var key in sections)
         if (sections[key])
@@ -18,23 +51,9 @@ function main() {
     gs.info('GoogleNewsRequestForm 0: Selected sections: ' + titles.join(', '));
 
     try {
-        var client = new sn_ws.RESTMessageV2('OkSurfNews', 'PostNewsSections');
-        var payload = JSON.stringify({
-            "sections": titles
-        });
-        client.setRequestBody(payload);
-        var resp = client.execute();
-        // load JSON into object if needed:
-        // var response = JSON.pasre(resp.getBody());
-        var attachment = new Attachment();
-        attachment.write(
-            current.getTableName(),
-            current.sys_id,
-            current.number + '.json',
-            'text/plain',
-            resp.getBody()
-        );
-
+        var response = getNews(titles);
+        writeAttachment(response);
+        writeNewsTableEntry();
         current.work_notes = 'Attached latest news feed to ' + current.number;
         gs.info('GoogleNewsRequestForm 1: attached news feed');
     } catch (err) {
